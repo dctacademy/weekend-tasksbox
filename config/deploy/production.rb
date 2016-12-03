7c4f83906f14b80c170e1a0a1eb2647ed77d6b49
= <ani@dctacademy.com>
2016-12-03 13:29:38 +0530

    without seed in deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -63,5 +63,5 @@ namespace :deploy do
    after  :finishing,    :compile_assets
    after  :finishing,    :cleanup
    after  :finishing,    :restart
-   after  :initial,      :seed
+   #after  :initial,      :seed
 end
